20f6d177ef3c04369bd3d99245b3aca1666dc9e9
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2019-07-08 22:07:44 +0200

    Make simple model -> table view rx binding

--- Core/Core/Screens/CookingViewController.swift
+++ Core/Core/Screens/CookingViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import RxSwift
+import RxRelay
+import RxCocoa
 
 public protocol CookingViewModelProtocol {
     var input: CookingViewModelProtocolInputs { get }
@@ -19,9 +21,36 @@ public protocol CookingViewModelProtocolInputs {
 }
 
 public protocol CookingViewModelProtocolOutputs {
+    var steps: BehaviorRelay<[String]> { get }
+}
+
+class CookingViewModel: CookingViewModelProtocol, CookingViewModelProtocolInputs, CookingViewModelProtocolOutputs {
+    var input: CookingViewModelProtocolInputs { return self }
+    var output: CookingViewModelProtocolOutputs { return self }
     
+    var steps = BehaviorRelay<[String]>(value: [])
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+            self?.steps.accept(["Buy stuff 🤑", "Cook 🤩", "Eat 🤪"])
+        }
+    }
 }
 
 public class CookingViewController: UITableViewController {
+    private let disposeBag = DisposeBag()
+    var viewModel: CookingViewModelProtocol = CookingViewModel()
     
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.output.steps
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
+        viewModel.output.steps.bind(to: tableView.rx.items(cellIdentifier: "CellID")) { row, model, cell in
+                cell.textLabel?.text = model
+            }
+            .disposed(by: disposeBag)
+    }
 }
