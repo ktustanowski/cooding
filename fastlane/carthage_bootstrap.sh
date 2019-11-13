30ea538727c5e18f2934afb25776873d3fc2f92f
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2019-11-13 23:51:25 +0100

    Update and tweak carthage

--- fastlane/carthage_bootstrap.sh
+++ fastlane/carthage_bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-carthage bootstrap --platform iOS
+carthage bootstrap --platform iOS --no-use-binaries
 cd Core
-carthage bootstrap --platform iOS
+carthage bootstrap --platform iOS --no-use-binaries
 cd ..
